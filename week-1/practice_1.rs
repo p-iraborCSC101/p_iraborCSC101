ede203fcee91c868e579ddcf95e20ba30c2a4587
p-iraborCSC101 <paula.irabor@pau.edu.ng>
2022-11-11 17:43:43 +0100

    (My First Commit)

---
+++ week-1/practice_1.rs
@@ -0,0 +1,9 @@
+fn main() {
+	println!("Welcome to CSC 101!");
+	let paula = narcissist;
+
+
+	println!("paula is such a {}",paula );
+	
+}
+
---
+++ week-2/practice_2.rs
@@ -0,0 +1,14 @@
+fn main() {
+	
+	println!(); // prints just a
+
+	println!("hello ");//prints
+
+	println!("format {} arguments", "some" ); //prints formats some arguments
+
+	print!("My name is {}. I am not the class rep of stream 2","Irabor Paula" );
+
+}
+
+
+
