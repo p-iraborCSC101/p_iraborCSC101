e43fe3088afaefb3e0112a291ff3e89098dbdbfc
p-iraborCSC101 <paula.irabor@pau.edu.ng>
2023-02-08 16:14:16 +0100

    (my sixth commit)

--- week-5/project_3/src/main.rs
+++ week-5/project_3/src/main.rs
@@ -7,6 +7,10 @@ fn main() {
 	let food_type:&str = input1.trim().parse().expect("failed to read input");
 
 	let mut input2 = String::new();
+	println!("Please input the quantity of food");
+	io::stdin().read_line(&mut input2).expect("failed to read input");
+	let quantity:&str = input2.trim().parse().expect("failed to read input");
+
 
 	
 }
